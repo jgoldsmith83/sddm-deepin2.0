dd61f69d2bfd521e532670e7e8525c1571877bb8
jgoldsmith83 <mapexmelee@gmail.com>
2016-07-21 01:26:47 -0400

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo cp -r deepin/ /usr/share/sddm/themes/
-sudo sed -i "s/^Current=.*/Current=deepin/g" /etc/sddm.conf
+sudo cp -r deepin2.0/ /usr/share/sddm/themes/
+sudo sed -i "s/^Current=.*/Current=deepin2.0/g" /etc/sddm.conf
 echo "Theme has been installed. Enjoy it!"
+echo "Please send thanks to Match-Yang at yangwanqing.me@qq.com"
